13a035691adeaa7f2890bfbb958ceeccb4200ac7
Andrés Pérez López <contact@andresperezlopez.com>
2015-12-14 00:19:20 +0100

    structured handlers

--- myo_raw_osc.py
+++ myo_raw_osc.py
@@ -1,10 +1,9 @@
 # # Usage
-#   python myo_raw_osc.py -i/--ip -p/--port -s/--send -e/--emgOn -u/--imuOn
+#   python myo_raw_osc.py -s/--send -i/--ip -p/--port
+#   - -s: Send (1) or not (0) data through OSC. Default to 0.
 #   - -i: IP destination address. Default to 127.0.0.1
 #   - -p: UDP destination port. Default to 57120 (sclang)
-#   - -s: Send (1) or not (0) data through OSC. Default to 1.
-#   - -e: Get (1) or not (0) EMG data
-#   - -u: Get (1) or not (0) IMU data
+
 #
 # # OSC messages:
 #   - [/myo/emg, (8 values with raw EMG data)]
@@ -35,13 +34,13 @@ imuOn = 1
 
 ### get command-line options
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"s:hi:p:e:u:",
-        ["send=","ip=","port=","emgOn=","imuOn"])
+    opts, args = getopt.getopt(sys.argv[1:],"s:hi:p:",
+        ["send=","ip=","port="])
 except getopt.GetoptError:
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print ('myo_raw_osc.py -s <sendOn> -i <dest IP> -p <dest port> -e <emgOn> -u <imuOn>')
+        print ('myo_raw_osc.py -s <sendOn> -i <dest IP> -p <dest port>')
         sys.exit()
     elif opt in ("-s", "--send"):
         send = int(arg)
@@ -49,56 +48,67 @@ for opt, arg in opts:
         ip = arg
     elif opt in ("-p", "--port"):
         port = int(arg)
-    elif opt in ("-e", "--emgOn"):
-        emgOn = int(arg)
-    elif opt in ("-u", "--imuOn"):
-        imuOn = int(arg)
 
 
 ### init
 m = MyoRaw()
-client = OSC.OSCClient()
-client.connect( (ip, port) )
+orientation=[]
+
 
 ### define handlers
-def proc_emg(emg, moving):
-    print(emg,moving)
-    if send:
-        try:
-            msg = OSC.OSCMessage()
-            msg.setAddress("/myo/emg")
-            msg.append(emg)
-            client.send(msg)
-        except OSC.OSCClientError:
-            print('ERROR: Client %s %i does not exist' % (ip, port))
-            sys.exit()
 
-def proc_imu(quat, gyro, acc): # acc and gyro values were changed
-    # gyro : [pitch/elevation, roll, yaw/azimuth]
-
+###### orientation transform
+def proc_imu_transform(quat, gyro, acc):
+    global orientation
+     # orientation : [yaw/azimuth, roll, pitch/elevation]
     orientation = transforms3d.taitbryan.quat2euler(quat)
+
+
+###### verbose
+def proc_emg_verb(emg,moving):
+    print(emg,moving)
+    
+def proc_imu_verb(quat, gyro, acc): # acc and gyro values were changed
+    # gyro : [pitch/elevation, roll, yaw/azimuth]
     # orientation : [yaw/azimuth, roll, pitch/elevation]
     print(orientation,acc)
-    if send:
-        try:
-            msg = OSC.OSCMessage()
-            msg.setAddress("/myo/imu")
-            msg.append(orientation)
-            msg.append(acc)
-            client.send(msg)
-        except OSC.OSCClientError:
-            print('ERROR: Client %s %i does not exist' % (ip, port))
-            sys.exit()
+    
+    
+###### OSC
+def proc_emg_osc(emg, moving):
+    try:
+        msg = OSC.OSCMessage()
+        msg.setAddress("/myo/emg")
+        msg.append(emg)
+        client.send(msg)
+    except OSC.OSCClientError:
+        print('ERROR: Client %s %i does not exist' % (ip, port))
+        sys.exit()
+
+def proc_imu_osc(quat, gyro, acc):
+    try:
+        msg = OSC.OSCMessage()
+        msg.setAddress("/myo/imu")
+        msg.append(orientation)
+        msg.append(acc)
+        client.send(msg)
+    except OSC.OSCClientError:
+        print('ERROR: Client %s %i does not exist' % (ip, port))
+        sys.exit()
 
 
 
 ### main process
 m.connect()
-if emgOn:
-    m.add_emg_handler(proc_emg)
-if imuOn:
-    m.add_imu_handler(proc_imu)
-
+m.add_emg_handler(proc_imu_transform)
+m.add_emg_handler(proc_emg_verb)
+m.add_imu_handler(proc_imu_verb)
+if send:
+    client = OSC.OSCClient()
+    client.connect( (ip, port) )
+    m.add_emg_handler(proc_emg_osc)
+    m.add_imu_handler(proc_imu_osc)
+     
 # m.add_arm_handler(lambda arm, xdir: print('arm', arm, 'xdir', xdir))
 # m.add_pose_handler(lambda p: print('pose', p))
 
